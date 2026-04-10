6badec8440561836c1e8989af15df84ec9f0928c
Meherab Hossen <mhnnishat0102@gmail.com>
2026-04-10 20:09:53 +0600

    requirements

---
+++ lib/todo_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TodoApp extends StatefulWidget {
+  const TodoApp({super.key});
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/z_readme.txt
@@ -0,0 +1,12 @@
+1. Create an app for manage your todo app.
+
+2. Requirements:
+Id
+Title
+Descroption
+Status(Pending, in progress, completed)
+CreatedAt(Time)
+
+3. List will hold the todos.
+
+4. New screen for create todos
